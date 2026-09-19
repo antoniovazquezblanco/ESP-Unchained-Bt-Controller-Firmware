5196d0419e708b6933f159617e6c3234b53338a1
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2026-09-19 10:14:18 +0200

    ESP32_BT_ROM: Add a new component

---
+++ components/esp32_bt_rom/README.md
@@ -0,0 +1,3 @@
+# ESP32 BT ROM
+
+Typed declarations for the ESP32 classic (BR/EDR) Bluetooth controller's ROM functions.
---
+++ components/esp32_bt_rom/include/esp32_bt_rom.h
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Typed declarations for the ESP32 classic (BR/EDR) Bluetooth controller's ROM
+ * functions, for hooking and patching. ESP32 only.
+ */
+
+#ifndef ESP32_BT_ROM_H
+#define ESP32_BT_ROM_H
+
+#endif /* ESP32_BT_ROM_H */
