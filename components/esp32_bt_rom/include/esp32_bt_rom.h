145ba18e6065577a88b7785d5483630e822bdd41
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2026-09-22 23:35:39 +0200

    ESP32_BT_ROM: Add company id variable location.

--- components/esp32_bt_rom/include/esp32_bt_rom.h
+++ components/esp32_bt_rom/include/esp32_bt_rom.h
@@ -9,9 +9,21 @@
 #define ESP32_BT_ROM_H
 
 #include <stdbool.h>
+#include <stdint.h>
 
 #include "r_ip_funcs.h"
 #include "r_modules_funcs.h"
+
+/**
+ * Local controller Bluetooth SIG Company Identifier (CompId), default 0x0060.
+ *
+ * A ROM-data constant (never written by the controller) reported in the HCI
+ * Read_Local_Version_Information event and in the LMP/LL version exchange, so
+ * writing it re-brands the controller both to the host and over the air. Provided
+ * as an absolute symbol by esp32_bt_rom.ld; assign a new value once after
+ * esp_bt_controller_enable() to fingerprint a ROM-modified controller.
+ */
+extern volatile uint16_t co_default_compid;
 
 /**
  * A self-test function for the ESP32 Bluetooth ROM functions.
