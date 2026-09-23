e2801c866e8cff28446dcd059597263c4f3389b0
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2026-09-23 15:47:15 +0200

    BT_PROCOLS: Add more HCI definitions.

--- components/bt_protocols/include/hci.h
+++ components/bt_protocols/include/hci.h
@@ -8,6 +8,8 @@
 #ifndef HCI_H
 #define HCI_H
 
+#include <stdint.h>
+
 /* HCI packet type: the first byte of every frame (Vol 4, Part A). */
 #define HCI_PKT_CMD 0x01
 #define HCI_PKT_ACL 0x02
@@ -23,8 +25,24 @@
 /* The top two bits of an ISO length are reserved. */
 #define HCI_ISO_LEN_MASK 0x3FFF
 
+/* A command opcode packs a 6 bit group (OGF) and a 10 bit command (OCF). */
+#define HCI_OPCODE(ogf, ocf) ((uint16_t)(((ogf) << 10) | ((ocf) & 0x03FF)))
+#define HCI_OPCODE_OGF(opcode) (((opcode) >> 10) & 0x003F)
+#define HCI_OPCODE_OCF(opcode) ((opcode) & 0x03FF)
+
+/* Opcode groups (Vol 4, Part E 5.4.1). */
+#define HCI_OGF_LINK_CTRL 0x01
+#define HCI_OGF_LINK_POLICY 0x02
+#define HCI_OGF_CTRL_BASEBAND 0x03
+#define HCI_OGF_INFO_PARAMS 0x04
+#define HCI_OGF_STATUS_PARAMS 0x05
+#define HCI_OGF_TESTING 0x06
+#define HCI_OGF_LE_CTRL 0x08
+#define HCI_OGF_VENDOR 0x3F
+
 /* Command status / error codes (Vol 1, Part F). */
 #define HCI_SUCCESS 0x00
+#define HCI_ERR_UNKNOWN_HCI_CMD 0x01
 #define HCI_ERR_INVALID_PARAMS 0x12
 
 #endif /* HCI_H */
